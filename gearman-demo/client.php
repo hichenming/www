abcb6403a41c30a252dd2d9e10aeedda5ab986de
hichenming <hichenming@gmail.com>
2012-08-15 22:29:12 +0800

    添加gearman与PHP和node.js结合的示例

---
+++ gearman-demo/client.php
@@ -0,0 +1,7 @@
+<?php
+  $client= new GearmanClient();
+  $client->addServer("127.0.0.1", 4730);
+  $client->do('sendmsg', $_SERVER['REMOTE_ADDR']);
+  //print $client->do("title", "Linvo");
+  //print "/n";
+?>
---
+++ gearman-demo/worker.js
@@ -0,0 +1,18 @@
+var Gearnode = require("gearnode");
+var io = require('socket.io').listen(8000);
+io.sockets.on('connection', function(socket){
+    socket.on('chat', function(data){
+        socket.broadcast.emit('chat', {
+            'msg': data
+        });
+    });
+});
+
+worker = new Gearnode();
+worker.addServer(); // use localhost
+
+worker.addFunction("sendmsg", "utf-8", function(payload, job){
+    var response =  payload.toUpperCase();
+    io.sockets.emit('chat', {'msg':response});
+    job.complete(response);
+});
