841b3f68247b7e355c58a1b896adfce40532bc83
root <root@li475-136.members.linode.com>
2012-08-16 06:40:26 +0000

    设置socket.io最小化js

--- gearman-demo/client.php
+++ gearman-demo/client.php
@@ -1,7 +1,8 @@
 <?php
   $client= new GearmanClient();
   $client->addServer("127.0.0.1", 4730);
-  $client->do('sendmsg', $_SERVER['REMOTE_ADDR']);
+  //$client->do('sendmsg', $_SERVER['REMOTE_ADDR']);
+  $client->doBackground('sendmsg', $_SERVER['REMOTE_ADDR']);
   //print $client->do("title", "Linvo");
   //print "/n";
 ?>
