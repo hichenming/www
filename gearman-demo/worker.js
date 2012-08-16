7a85598f95339fb5da969d6054cb9ba7a7e59188
root <root@li475-136.members.linode.com>
2012-08-16 06:42:12 +0000

    min worker.js

--- gearman-demo/worker.js
+++ gearman-demo/worker.js
@@ -1,5 +1,6 @@
 var Gearnode = require("gearnode");
 var io = require('socket.io').listen(8000);
+io.set('browser client minification', true);
 io.sockets.on('connection', function(socket){
     socket.on('chat', function(data){
         socket.broadcast.emit('chat', {
